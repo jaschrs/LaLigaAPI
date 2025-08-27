ae2364a3617f9f0982346fbe6064dbf9100334d5
Jasper <jasperjchristian22@gmail.com>
2025-08-27 16:27:14 -0400

    Remove sensitive xmaskey value
    Replaced xmaskey value with an empty string for security.

--- app/public.py
+++ app/public.py
@@ -7,7 +7,7 @@ class PublicKey:
     fotmobplayers: str = "https://www.fotmob.com/api/data/playerData?id="
     fotmobtable: str = "https://www.fotmob.com/api/data/tltable?leagueId=87"
     fotmoblaliga: str = "https://www.fotmob.com/api/data/leagues?id=87"
-    xmaskey: str = "eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9wbGF5ZXJEYXRhP2lkPTk5MDk5NiIsImNvZGUiOjE3NTYzMjEzOTY4NDcsImZvbyI6InByb2R1Y3Rpb246ODBmOTc5OWM5ZThkZTI5ZGE5NjhhNDkxYWY3MmZmMjY3OWU2NWY0ZSJ9LCJzaWduYXR1cmUiOiI0OUE3QkVBQjlEMENBNTAyNjU0MTNERUUxNjc0MEVGQiJ9"
+    xmaskey: str = ""
     headers: dict = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
