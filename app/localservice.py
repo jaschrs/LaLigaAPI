0263f24ef1937b717239f24e10bba6e1813c90fc
jaschrs <jasperjchristian22@gmail.com>
2025-08-27 01:43:06 -0400

    added comments (@jaschrs)

--- app/localservice.py
+++ app/localservice.py
@@ -5,6 +5,7 @@ def config_local_service():
     Configures the local Flask service.
     :return: Flask app instance with JSON sorting disabled
     """
+
     app = Flask(__name__)
     app.json.sort_keys = False
     return app
